fe127841824a15efae4150e908fa3327dc160e11
Chris <c.lavender@pm.me>
2023-10-09 18:12:44 +0100

    update

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -99,7 +99,9 @@ public partial class GamePage : ContentPage
         // stub to show the program is using the correct letter against the word returned in SelectWord function
         if (remainingAttempts > 0 && isCorrect)
 		{
-			await DisplayAlert("Good", letter.ToString() + " is in word", "OK");			
+			await DisplayAlert("Good", letter.ToString() + " is in word", "OK");	
+			
+			/// this needs to update the display
 		}
 
 		else
